4c2993452dae20b3a8ee3c0784bef7276032701f
MARTTELO <marttelo@gmail.com>
2026-02-16 20:12:03 -0300

    corrigindo identação

--- src/class/user.php
+++ src/class/user.php
@@ -8,29 +8,34 @@ class User
         $this->pdo = $pdo;
     }
 
-    public function create($name, $email){
+    public function create($name, $email)
+    {
         $sql = "INSERT INTO users(name, email) VALUES (:name, :email)";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute(['name'=> $name, 'email'=> $email]);
+        $stmt->execute(['name' => $name, 'email' => $email]);
 
 
     }
 
-    public function read($id){
+    public function read($id)
+    {
         $sql = 'SELECT * FROM users';
         $stmt = $this->pdo->query($sql);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update($id, $name, $email){
+    public function update($id, $name, $email)
+    {
         $sql = 'UPDATE users SET name = :name, email = :email WHERE id = :id';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute(['id' => $id, 'name'=> $name,'email'=> $email]);
+        $stmt->execute(['id' => $id, 'name' => $name, 'email' => $email]);
 
-}
+    }
 
-public function delete($id){
-    $sql = 'DELETE users WHERE id = :id';
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute(['id'=> $id]);
+    public function delete($id)
+    {
+        $sql = 'DELETE users WHERE id = :id';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+    }
 }
