1188c7e7e3b82f4011418e5309bf28444cdf9a7e
MARTTELO <marttelo@gmail.com>
2026-02-16 22:00:50 -0300

    .

--- config/db.php
+++ config/db.php
@@ -5,8 +5,7 @@ $username = 'root';
 $password = '';
 
 try {
-  $conn = new PDO('mysql:host=localhost;dbname=crud_php_pdo', username: $username, password: $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", username: $username, password: $password);
 } catch (PDOException $e) {
    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
 
--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 require_once __DIR__ . "/config/db.php";
 require_once __DIR__ . "/src/controllers/UserController.php";
 
+$userController = new UserController($pdo);
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['create'])){
+      $userController->create($_POST['name'], $_POST['email']);
+    }elseif(isset($_POST['update'])){
+
+    }elseif(isset($_POST['delete'])){
+  }
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -11,17 +21,43 @@ require_once __DIR__ . "/src/controllers/UserController.php";
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
+  <link rel="stylesheet" href="public\css\style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 
 <body>
-  <h1>Usuários</h1>
-  <form class="formController" action="" id="createForm" method="POST">
-    <input type="text" id="createName" name="name" placeholder=" Nome">
-    <input type="email" id="createEmail" name="email" placeholder="E-mail">
-    <button type="submit" name="create">Adicionar</button>
+
+
+  <form id="createForm" method="POST">
+    <h1>Usuários</h1>
+    <div class="mb-3">
+      <label for="name" class="form-label">Nome</label>
+      <input type="text" class="form-control" id="createName" name="name" placeholder="Nome">
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">Email</label>
+      <input type="email" class="form-control" id="createEmail" aria-describedby="emailHelp">
+    </div>
+    <button type="submit" name="creae" class="btn btn-primary">Adicionar</button>
   </form>
+
+  <table>
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>Email</td>
+            <td>Ações</td>
+        </tr>
+    </thead>
+    <tbody>
+      <?php       
+      
+      ?>
+    </tbody>
+  </table>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
